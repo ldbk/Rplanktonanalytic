1c5dc7aa8afaa5b12b8c977c597a1ff7afc0689b
ldbk <laurent.dubroca@gmail.com>
2019-10-29 16:53:52 +0100

    spline_point updated

--- R/spline_points.R
+++ R/spline_points.R
@@ -1,35 +1,49 @@
-#' Identification of bloom descriptors dates based on smoothing splines
+#' spline_points
+#'
+#'Identification of bloom descriptors dates based on smoothing splines
 #' 
-#' @param x A dataframe containing the temporal information and the abundance of a species.
-#' @param s_param The smoothing parameter (default 0.35).
-#' @return A new dataframe containing temporal and species abundance info and date of start, maximum abundance and end of the bloom of the species.
+#' @param value a numerical vector of values (abundances...) 
+#' @param time a numerical vector of temporal units (month, week...)
+#' @param s_param the smoothing parameter corresponding to the spar parameters
+#' of the smooth.spline function (default 0.35).
+#'
+#' @return a dataframe containing the time vector, the value vector and info
+#' a character vector with 'Start', 'Max' and 'End' for the seasonal peak and NA
+#' for the other date
 #' @examples
 #' \dontrun{
-#' spline.points(x,s_param=0.35)
+#'	  time<-1:12
+#'	  value<-c(0,0,1,2,5,7,3,0,0,0,0,0)
+#' 	  spline.points(time,value,s_param=0.35)
 #' }
 #' @export
 #' 
-spline_points <- function(x,s_param=0.35){
+spline_points <- function(value,time,s_param=0.35){
   
-  smt <- stats::smooth.spline(x$day, x$sp,spar=s_param)
+  smt <- stats::smooth.spline(time, value,spar=s_param)
   smt_pre <- smt$y
   smt_der <- stats::predict(smt,deriv = 1)
   
-  smt_df <- cbind(x,smt_pre)
+  smt_df <- data.frame(time,value,smt_pre)
   smt_df$der <- smt_der$y
+  maxder<-max(smt_df$der)
+  minder<-min(smt_df$der)
+  maxderpos<-maxder>0
+ 
+  smt_df$info<-NA_character_
+  smt_df$info[(smt_df$der>=maxder)&(maxderpos)]<-"Start"
+  smt_df$info[(smt_df$der<=minder)&(maxderpos)]<-"End"
+  smt_df$info[(smt_df$value>=max(smt_df$value))&(max(value)>0)]<-"Max"
   
-  output=
-    smt_df %>% mutate(info = case_when(
-      der>=max(der) & max(der)>0
-      # & ab_treshold=tres
-      ~ "Start",
-      der<=min(der) & max(der)>0 ~ "End",
-      sp>=max(sp) & max(sp)>0 ~ "Max",
-      TRUE ~ NA_character_))
+  #output=
+  #  smt_df %>% mutate(info = case_when(
+  #    der>=max(der) & max(der)>0
+  #    # & ab_treshold=tres
+  #    ~ "Start",
+  #    der<=min(der) & max(der)>0 ~ "End",
+  #    value>=max(value) & max(value)>0 ~ "Max",
+  #    TRUE ~ NA_character_))
   
-  outoput <- output %>% select(date:sp,info)
-  return(output)
-}
+  return(smt_df)
 
-?smooth.spline
-?predict
+}
