bfc11eea4603c22f7e1b1b967293c1d74e825e11
langbart <lorenzo.longobardi@gmail.com>
2019-10-31 12:12:30 +0100

    added the argument "t_scale" useful for the selection of the temporal resolution of the data

--- R/spline_uni.R
+++ R/spline_uni.R
@@ -9,6 +9,8 @@
 #' @param ab_treshold quantile of the positive abundance distribution (default 0.75)
 #' @param obs_year minimum number of samples for each year in which the species shows an abundance>0 (default 2)
 #' @param s_param the smoothing parameter corresponding to the spar parameters
+#' @param t_scale temporal scale of the data, 52 for weekly data, 12 for monthly data (default 52)
+
 #' of the smooth.spline function (default 0.35).
 #' @param S.NAME name of the species column
 #' 
@@ -24,19 +26,19 @@
 #' }
 #' @export
 #' 
-spline_uni<- function(x,ab_treshold=0.75,obs_year=2,s_param=0.35,S.NAME=""){
+spline_uni<- function(x,ab_treshold=0.75,obs_year=2,s_param=0.35,t_scale=52,S.NAME=""){
   ## 1: remove NAs keep robust years
   x <- x %>% select(1:7,S.NAME)
   colnames(x)[ncol(x)]="value"
   df1 <- na.omit(x)
   ## 2: keep years containing N>=75% of possible observations
   ok_years <- 
-    df1  %>% group_by(year) %>% tally() %>% filter(n>(52*0.75)) %>% pull(year) 
+  df1  %>% group_by(year) %>% tally() %>% filter(n>(t_scale*0.75)) %>% pull(year) 
   df2 <- df1 %>% filter(year %in% ok_years)
   ## 3: consider only years in which species has/have at least X observations > 0 
   year_ob <- 
-    df2  %>% filter(value>0) %>% group_by(year) %>%
-    tally() %>% filter(n>obs_year) %>% pull(year) 
+  df2  %>% filter(value>0) %>% group_by(year) %>%
+  tally() %>% filter(n>obs_year) %>% pull(year) 
   df3 <- df2 %>% filter(year %in% year_ob) %>% droplevels()
   y_levels <- unique(df3$year) 
   ## 4: calculate bloom abundance treshold 
