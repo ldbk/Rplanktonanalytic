aadd8c8ce47217cf2a918b7ab17528dca8b79145
langbart <lorenzo.longobardi@gmail.com>
2021-06-30 19:34:03 +0200

    Adding argument "past" to eventually pick the obsertvaiton before the max derivative. Drop unused fucntions, clean code layout

--- R/spline_multi.R
+++ R/spline_multi.R
@@ -1,10 +1,9 @@
 #' spline_multi
 #'
+#' Identification of annual bloom descriptors dates based on smoothing splines for all the species of the dataframe.
+#' The function automatically exludes those years containing less than  75% of
+#' the possible observations for the temporal scale considered.
 #'
-#'Identification of annual bloom descriptors dates based on smoothing splines for all the species of the dataframe. 
-#'The function automatically exludes those years containing less than  75% of
-#'the possible observations for the temporal scale considered.
-#' 
 #' @param x a dataframe having the first 7 columns named as "location", "station", "date", "year", "month", "week", "day",
 #' other columns corresponds to the species columns
 #' @param ab_treshold quantile of the positive abundance distribution (default 0.75)
@@ -12,27 +11,30 @@
 #' @param s_param the smoothing parameter corresponding to the spar parameters
 #' of the smooth.spline function (default 0.35).
 #' @param t_scale temporal scale resolution of the data, 52 for weekly data, 12 for monthly data (default 52)
-#' 
+#' @param control control the flexibility of the function (default 0)
+#' @param past whether want to pick the observation just before max derivative
+#'
 #' @return a dataframe containing the species names, the annual time vector, the value vector and info
 #' a character vector with 'Start', 'Max' and 'End' for the seasonal peak and NA
 #' for the other date
 #' @examples
 #' \dontrun{
-#'	  data("phytopknar")
-#'    phytopknar_ret <- ret_time(phytopknar)
-#'    phytopknar_ret_ord <- phytopknar_ret %>% dplyr::select(location,station,date,year,month,week,day,everything())
-#'    spline_multi(phytopknar_ret_ord,ab_treshold=0.75,obs_year=2,s_param=0.35,t_scale=52)
+#' data("phytopknar")
+#' phytopknar_ret <- ret_time(phytopknar)
+#' phytopknar_ret_ord <- phytopknar_ret %>% dplyr::select(location, station, date, year, month, week, day, everything())
+#' spline_multi(phytopknar_ret_ord, ab_treshold = 0.75, obs_year = 2, s_param = 0.35, t_scale = 52, control = 0)
 #' }
 #' @export
-#' 
-spline_multi<- function(x,ab_treshold=0.75,obs_year=2,s_param=0.35,t_scale=52){
-  df_emp2 <- data.frame()
-  for ( j in 8:ncol(x) ) {
-    S.NAME <- names(x[j])
-    df1.1 <- x %>% select(1:7,j)
-    
-    df2.1 <- spline_uni(df1.1,ab_treshold=ab_treshold,obs_year=obs_year,s_param=s_param,t_scale=t_scale,S.NAME=S.NAME)
-    df_emp2 <- rbind(df_emp2,df2.1) %>% as.data.frame()
+#'
+spline_multi <-
+  function(x, ab_treshold = 0.75, obs_year = 2, s_param = 0.35, t_scale = 52, control = 0, past = TRUE) {
+    df_emp2 <- data.frame()
+    for (j in 8:ncol(x)) {
+      S.NAME <- names(x[j])
+      df1.1 <- x %>% select(1:7, j)
+
+      df2.1 <- spline_uni(df1.1, ab_treshold = ab_treshold, obs_year = obs_year, s_param = s_param, t_scale = t_scale, S.NAME = S.NAME, control = control)
+      df_emp2 <- rbind(df_emp2, df2.1) %>% as.data.frame()
+    }
+    return(df_emp2)
   }
-  return(df_emp2)
-}
--- R/spline_points.R
+++ R/spline_points.R
@@ -1,58 +1,91 @@
 #' spline_points
 #'
-#'Identification of bloom descriptors dates based on smoothing splines
-#' 
-#' @param value a numerical vector of values (abundances...) 
+#' Identification of bloom descriptors dates based on smoothing splines
+#'
+#' @param value a numerical vector of values (abundances...)
 #' @param time a numerical vector of temporal units (month, week...)
 #' @param s_param the smoothing parameter corresponding to the spar parameters
 #' of the smooth.spline function (default 0.35).
+#' @param control control the flexibility of the function (default 0).
+#' @param past whether want to pick the observation just before max derivative
 #'
 #' @return a dataframe containing the time vector, the value vector and info
 #' a character vector with 'Start', 'Max' and 'End' for the seasonal peak and NA
 #' for the other date
 #' @examples
 #' \dontrun{
-#'	  time<-1:12
-#'	  value<-c(0,0,1,2,5,7,3,0,0,0,0,0)
-#' 	  spline_points(value,time,s_param=0.35)
+#' time <- 1:12
+#' value <- c(0, 0, 1, 2, 5, 7, 3, 0, 0, 0, 0, 0)
+#' spline_points(value, time, s_param = 0.35, control = 0)
 #' }
 #' @export
-#' 
-spline_points <- function(value,time,s_param=0.35){
-#	if(F){
-#	  time<-1:12
-#	  value<-c(0,0,1,2,5,7,3,0,0,0,0,0)
-#	  spline_points(value,time,s_param=0.35)
-#  	smt <- stats::smooth.spline(time, value,spar=0.35)
-#	  plot(time,value)
-#	lines(predict(smt))
-#	lines(predict(smt,deriv=1))
-#	  plot(time,value)
-#	}
-  
-  smt <- stats::smooth.spline(time, value,spar=s_param)
-  smt_pre <- smt$y
-  smt_der <- stats::predict(smt,deriv = 1)
-  
-  smt_df <- data.frame(time,value,smt_pre)
-  smt_df$der <- smt_der$y
-  maxder<-max(smt_df$der)
-  minder<-min(smt_df$der)
-  maxderpos<-maxder>0
- 
-  smt_df$info<-NA_character_
-  smt_df$info[(smt_df$der>=maxder)&(maxderpos)]<-"Start"
-  smt_df$info[(smt_df$der<=minder)&(maxderpos)]<-"End"
-  smt_df$info[(smt_df$value>=max(smt_df$value))&(max(smt_df$value)>0)]<-"Max"
-  
-  #output=
-  #  smt_df %>% mutate(info = case_when(
-  #    der>=max(der) & max(der)>0
-  #    # & ab_treshold=tres
-  #    ~ "Start",
-  #    der<=min(der) & max(der)>0 ~ "End",
-  #    value>=max(value) & max(value)>0 ~ "Max",
-  #    TRUE ~ NA_character_))
-  
-  return(smt_df)
-}
+#'
+spline_points <-
+  function(value, time, s_param = 0.4, control = 0, past = TRUE) {
+    smt <- stats::smooth.spline(time, value, spar = s_param)
+    smt_pre <- smt$y
+    smt_der <- stats::predict(smt, deriv = 1)
+
+    smt_df <- data.frame(time, value, smt_pre)
+    smt_df$der <- smt_der$y
+    maxder <- max(smt_df$der)
+    minder <- min(smt_df$der)
+    maxderpos <- maxder > 0
+
+    tre_der <- maxder - (maxder * control)
+    if (isTRUE(past)) {
+      max_der_n <- which.max(smt_df$der) - 1
+    }
+    else {
+      max_der_n <- which.max(smt_df$der)
+    }
+    max_time <- smt_df[max_der_n, 1]
+    quant_der <- quantile(subset(smt_df, der > 0)$der, 0.9)
+    der_cond <- subset(smt_df, der >= tre_der & der >= quant_der & time < max_time)
+    time_ref <- der_cond[1, 1]
+
+    smt_df$info <- NA
+
+    # find start
+    if (!is.na(which(smt_df$time == time_ref)[1])) {
+      smt_df$info[which(smt_df$time == time_ref)] <- "Start"
+    } else {
+      smt_df$info[which((smt_df$der >= maxder) & (maxderpos))] <- "Start"
+    }
+
+    # find max
+    ref_max <- which(smt_df$info == "Start")
+    smt_df_max <- smt_df[ref_max:nrow(smt_df), ]
+    max_val <- max(smt_df_max$value)
+
+    max_cond <- subset(smt_df, smt_df$value == max_val & max_val > 0)
+    max_time_ref <- max_cond[nrow(max_cond), 1]
+    smt_df$info[which(smt_df$time == max_time_ref)] <- "Max"
+
+    # find end
+    ref_end <- which(smt_df$info == "Max")
+    tre_min <- quantile(subset(smt_df, value > 0)$value, 0.33)
+    n_obs <- round(nrow(smt_df) * 0.075)
+
+    smt_df_end <- smt_df[ref_end:nrow(smt_df), ]
+    minder <- min(smt_df_end$der)
+    smt_df_end$test_tremin <- ifelse(smt_df_end$value < tre_min, "T", "F")
+
+    y <- which(smt_df_end$test_tremin == "T")
+    y <- y + ref_end - 1
+    startIndx <- y[!(y - 1) %in% y]
+    stopIndex <- y[!(y + 1) %in% y] + 1
+    rows_seq_length <- rbind(startIndx, stopIndex)
+    potential_ends <- rows_seq_length[1, ]
+    ends_seq <- data.frame(t(rbind(potential_ends, diff(rows_seq_length))))
+    end_rows <- subset(ends_seq, stopIndex >= n_obs)
+
+    if (nrow(end_rows) >= 1) {
+      end_row <- min(end_rows$potential_ends)
+      smt_df$info[end_row] <- "End"
+    } else {
+      smt_df$info[which(smt_df$der == minder)] <- "End"
+    }
+
+    return(smt_df)
+  }
